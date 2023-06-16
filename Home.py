f190267fca6e96e8aa6ccb7ccd1e38db23c9417d
Sandesh K <sandesh@Sandeshs-MacBook-Air.local>
2023-06-17 01:25:39 +0530

    added logging

--- Home.py
+++ Home.py
@@ -8,7 +8,24 @@ import json
 import matplotlib.pyplot as plt
 import plotly.express as px
 import streamlit as st 
+import logging # Import the logging library
 #import streamlit.components.v1 as components
+
+import logging
+
+# Make sure the directory exists. If it doesn't, create it.
+import os
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+# Configure the logging module.
+logging.basicConfig(filename='logs/app.log', level=logging.INFO)
+
+
+
+
+logger = logging.getLogger(__name__)
+
 
 # Suppress warnings
 import warnings
@@ -16,23 +33,28 @@ warnings.filterwarnings('ignore', module='numpy')
 warnings.filterwarnings('ignore')
 st.title('s')
 
+logger.info('Starting to read CSV files') # Add a logging statement
 # Read the CSV files
 overall_submissions=pd.read_csv('All Submissions-main.csv')
 bounty_details=pd.read_csv('Bounty-main.csv')
 
+logger.info('Merging CSV files') # Add a logging statement
 # Merge the CSV files based on the 'Bounty' column
 overall_submissions=overall_submissions.merge(bounty_details,left_on='Bounty',right_on='Name')
 
+logger.info('Converting date format and sorting the dataframe') # Add a logging statement
 # Convert the 'End Date' column to datetime format and sort the dataframe by it
 overall_submissions['End Date']=pd.to_datetime(overall_submissions['End Date'])
 overall_submissions['End Date'] = overall_submissions['End Date'].dt.strftime("%d-%m-%Y %H:%M:%S")
 overall_submissions['End Date']=pd.to_datetime(overall_submissions['End Date'])
 overall_submissions=overall_submissions.sort_values(by='End Date',ascending=True)
 
+logger.info('Converting wallet columns to lowercase') # Add a logging statement
 # Convert 'MetaMask Wallet' and 'xMetric Wallet' columns to lowercase
 overall_submissions['MetaMask Wallet'] = overall_submissions['MetaMask Wallet'].str.lower()
 overall_submissions['xMetric Wallet'] = overall_submissions['xMetric Wallet'].str.lower()
 
+logger.info('Filtering out specific rows from overall_submissions')  # Add a logging statement
 # Filter out rows where 'Grading Observations v8' and 'Grading Observation v7' are NaN and 'Bounty' is not 'NEAR - 14. Wen Hunt? Now. '
 # we omit near- wen hunt cause it had -1 as score for everyone intentionally
 overall_submissions=overall_submissions[overall_submissions['Grading Observations v8'].isna()]
@@ -41,7 +63,7 @@ overall_submissions=overall_submissions[overall_submissions['Bounty']!='NEAR - 1
 overall_submissions_calc=overall_submissions.copy()
 
 
-
+logger.info('Creating a new dataframe for discord handles and sorting by Created At')  # Add a logging statement
 # Create a new dataframe for discord handles and sort by 'Created At'
 discord_handles=overall_submissions[['Discord Handle','xMetric Wallet','Created At']]
 discord_handles['Created At'] = pd.to_datetime(discord_handles['Created At'], format="%d/%m/%Y %H:%M")
@@ -52,54 +74,73 @@ discord_handles.drop_duplicates(subset='xMetric Wallet',keep='last',inplace=True
 
 
 
+logger.info('Fetching data from Flipside Crypto API')  # Add a logging statement
 # Fetch data from Flipside Crypto API
 on_chain_reviews=pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/37106c4d-afc1-49db-9412-b32a7b0a9ab8/data/latest')
 on_chain_bounty=pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/a40ccefa-b556-4dd0-b0a6-f38b494309b2/data/latest')
 on_chain_submission=pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/cd9455dd-7363-470a-8399-dcb7c5474842/data/latest')
+
+logger.info('Merging data from API responses')  # Add a logging statement
 # Merge data from API responses
 review_and_bounty=on_chain_reviews.merge(on_chain_bounty,left_on='BOUNTY_IDD',right_on='Searchable_id',how='left')
 submission_details=review_and_bounty.merge(on_chain_submission,on=['BOUNTY_ID','SUBMISSION_ID','ANALYST'],how='left')
 submissions=submission_details.merge(discord_handles,left_on='ANALYST',right_on='xMetric Wallet',how='left')
 
+logger.info('Filling missing values in Discord Handle with ANALYST')  # Add a logging statement
 # Fill missing values in 'Discord Handle' with 'ANALYST'
 submissions['xMetric Wallet']=submissions['ANALYST']
 submissions['Discord Handle'].fillna(submissions['ANALYST'],inplace=True)
 submissions['Chain/Project']=submissions['Chain/Project'].apply(lambda x:str(x))
 
 # Group the submissions dataframe
-
+logger.info('Grouping the submissions dataframe')  # Add a logging statement
 submissions=submissions[['SUBMISSION_URL','Challenge URL','Challenge','ANALYST','Discord Handle','Review Deadline','Chain/Project','SCORE']]
 submissions=submissions.groupby(by=['SUBMISSION_URL','Challenge URL','Challenge','ANALYST','Discord Handle','Review Deadline','Chain/Project'],as_index=False).mean()
 
 # Create a subset of overall_submissions_calc
+logger.info('Creating a subset of overall_submissions_calc')  # Add a logging statement
 overall_submissions_calc=overall_submissions_calc[['Bounty',  'Public Result(s)', 'Overall Avg. Grade', 'Discord Handle','End Date','Bounty Program Name','xMetric Wallet']]
 #Setting '-' as the challenge url for the challenges launched in notion and later setting it to the same name as bonuty.
 overall_submissions_calc['Challenge_url']='-'
 overall_submissions_calc.loc[overall_submissions_calc['Challenge_url'] == '-', 'Challenge_url'] = overall_submissions_calc['Bounty']
 
 # Create a subset of submissions and rename its columns
+logger.info('Creating a subset of submissions and renaming its columns')  # Add a logging statement
 submissions=submissions[['Challenge','SUBMISSION_URL', 'SCORE', 'Discord Handle','Review Deadline','Chain/Project','ANALYST','Challenge URL']]
 submissions.columns=['Bounty',  'Public Result(s)', 'Overall Avg. Grade', 'Discord Handle','End Date','Bounty Program Name','xMetric Wallet','Challenge_url']
 
 # Concatenate the dataframes and filter for certain 'Bounty Program Name' values
+logger.info('Concatenating the dataframes and filtering for certain Bounty Program Name values')  # Add a logging statement
 combined=pd.concat([overall_submissions_calc,submissions],axis=0,join='outer')
-# Bounty_Program_Name=combined['Bounty Program Name'].unique()
-Bounty_Program_Name=['Flow']
+
+logger.info('Filtering the dataset to include chain/project')  # Add a logging statement
+Bounty_Program_Name=combined['Bounty Program Name'].unique()
+# Bounty_Program_Name=['Flow']
 pattern = '|'.join(Bounty_Program_Name)
 processed_data = combined[combined['Bounty Program Name'].str.contains(pattern, na=False, case=False)]
 processed_data=combined.copy()
 
+logger.info('Exclude challenges with less than 2 participants')  # Add a logging statement
+# Get a list of challenge URLs where 'Discord Handle' count is less than 2
+challenge_urls_to_skip = processed_data.groupby(by='Challenge_url').filter(lambda x: x['Discord Handle'].nunique() < 2)['Challenge_url'].unique().tolist()
+
+# Filter the DataFrame to exclude the challenge URLs in the list
+processed_data = processed_data[~processed_data['Challenge_url'].isin(challenge_urls_to_skip)]
+
 # Initialize a dataframe for ELO score and save it to a CSV file
+logger.info('Initializing a dataframe for ELO score and saving it to a CSV file')  # Add a logging statement
 last_elo_score = pd.DataFrame(columns=['Discord Handle', 'mean', 'variance'])
 last_elo_score.to_csv('latest_elo_score.csv')
 
 # Initialize a list for final output 
 data_with_rank=[]
-# st.write(processed_data.tail())
+
 # Calculate ELO scores for each unique challenge url
+logger.info('Calculating ELO scores for each unique challenge url')  # Add a logging statement
 for bounty in processed_data['Challenge_url'].unique():
         # Remainder of the code includes calculations for ELO score
         # Filter for unique challenge url
+    logger.debug('Processing challenge: %s', bounty)  # Add a debugging statement
     record_details=[]
     
     challenge_df=processed_data[processed_data['Challenge_url']==bounty]
@@ -111,19 +152,16 @@ for bounty in processed_data['Challenge_url'].unique():
     latest_elo_score=pd.read_csv('latest_elo_score.csv')
     elo_score={}
     latest_elo_score=latest_elo_score[['Discord Handle','mean','variance']]
-    # st.write('before if-else')
+
 
     # Checking if challenge_url is same as bounty name, if it is, then it means it's a challenge launched on notion
     if challenge_df['Challenge_url'].iloc[0]==challenge_df['Bounty'].iloc[0]:
-        # st.write('inside if-else')
+
+        # continue
         
         
         challenge_df=challenge_df.drop_duplicates(subset=['Discord Handle'],keep='first')
-        # If the number of records in the challenge dataframe is less than 2, 
-        # continue to the next iteration of the loop without executing the following code.
-        # This is because we cannot compare or calculate ELO scores with less than two players.
-        if len(challenge_df)<2:
-            continue
+
 
         # Iterate over each 'Discord Handle' in the challenge data.
         for handle in (challenge_df['Discord Handle']):
@@ -145,7 +183,7 @@ for bounty in processed_data['Challenge_url'].unique():
         # Iterate over each ELO score in the elo_score dictionary.
         for elo_object_of_handle in elo_score:
             # Create a new Rating object for the ELO score and append it as a single-item tuple to the 'rating_tuple' list.
-            setup(backend='mpmath')
+            # setup(backend='mpmath')
             rating = Rating(elo_score[elo_object_of_handle])
             rating_tuple.append((rating,))
 
@@ -156,10 +194,8 @@ for bounty in processed_data['Challenge_url'].unique():
             
         # If there is an error while calculating the new ELO ratings, catch the exception and print out the details.
         except Exception as e:
-            print(challenge_df.Rank)
-            print(e)
-            print('new_ratings_problem',bounty)
-            print('z')
+            logger.error('Error calculating new ratings for challenge: %s. Error: %s', bounty, e)  # Add an error statement
+            continue
 
         # Set a counter to 0. This counter is used to index the new ratings.
         j=0
@@ -187,7 +223,7 @@ for bounty in processed_data['Challenge_url'].unique():
                     record_details.append(End_Date)
                     record_details.append(new_ratings[j][0].mu)
                     record_details.append(new_ratings[j][0].sigma)
-
+                    j=j+1
                 # If the 'Discord Handle' is present in the 'latest_elo_score' dataframe,
                 # update its ELO rating in the dataframe. Also append the details to the 'record_details' list.
                 else:
@@ -204,21 +240,27 @@ for bounty in processed_data['Challenge_url'].unique():
                     record_details.append(End_Date)
                     record_details.append(new_ratings[j][0].mu)
                     record_details.append(new_ratings[j][0].sigma)
+                    j=j+1
 
             # If there is an error while updating the ELO ratings or appending the details, catch the exception and print out the details.
-            except:
-                print('problem in elo_score_keys: ',bounty,len(elo_score.keys()),elo_score.keys())
+            except Exception as e:
+                logger.error('Error %s in updating elo_score for challenge: %s', e, bounty)  # Add an error statement
+                    
+                j=j+1
 
             # Append the record details to the 'data_with_rank' list. 
             # This list will later be used to create a dataframe with all the details.
             data_with_rank.append(record_details)
+        logger.debug('Successfully processed challenge: %s', bounty)  # Add a debugging statement
 
         # Save the 'latest_elo_score' dataframe to a CSV file.
-        #latest_elo_score.to_csv('latest_elo_score.csv')
+        # latest_elo_score.to_csv('latest_elo_score.csv')
 
         
     else:
-        # st.write('inside else')
+
+        # continue
+        logger.debug('Iterating over xMetric Wallet values in challenge_df')  # Add a debugging statement
 
         # Iterate over each value in the 'xMetric Wallet' column of the 'challenge_df' DataFrame.
         for i in (challenge_df['xMetric Wallet']):
@@ -228,13 +270,13 @@ for bounty in processed_data['Challenge_url'].unique():
             # The new rating is based on the 'mean' column of the 'latest_elo_score' DataFrame for the corresponding 'Discord Handle'.
             if i in latest_elo_score['Discord Handle'].values:
                 elo_score[i]=Rating(latest_elo_score[latest_elo_score['Discord Handle']==i]['mean'].values[0])
-                # st.write(1)
+
 
             # If the current value is not in the 'Discord Handle' column of the 'latest_elo_score' DataFrame,
             # assign a new rating of 100 to the 'elo_score' dictionary for that value.
             else:
                 elo_score[i]=Rating(100)
-                # st.write('2, gave elo')
+
 
 
         # Initialize an empty list 'rating_tuple'
@@ -248,22 +290,20 @@ for bounty in processed_data['Challenge_url'].unique():
 
             # Add the Rating object to the 'rating_tuple' list as a single-item tuple
             rating_tuple.append((rating,))
-            # st.write('create rating object')
+
 
         # Attempt to calculate new ratings based on the 'rating_tuple' list and ranks from the 'challenge_df' DataFrame
         try:
             new_ratings = rate(rating_tuple, ranks=challenge_df.Rank)
-            # st.write('calc rank')
+
 
         # If an exception occurs during the rating calculation,
         except Exception as e:
+            logger.error('Error calculating new ratings. Error: %s', e)  # Add an error statement
 
             # Print the exception message
-            print(e)
-            # Print a problem message along with the 'bounty' variable for context
-            print('Couldnt calc rank for',bounty)
+            # print(e)
 
-            # Skip the current iteration and move on to the next one
             continue
 
 
@@ -275,14 +315,13 @@ for bounty in processed_data['Challenge_url'].unique():
             
             # Initialize an empty list 'record_details'
             record_details=[]
-            # st.write('inside records loop')
+
 
             try:
                 # Check if current key is not in the 'Discord Handle' column of the 'latest_elo_score' DataFrame
-                st.write('entered try block')
+
                 if i not in latest_elo_score['Discord Handle'].values:
-                    st.write('try block entered successfully')
-                    st.write(challenge_df)
+
                     
                     # If not, create a new row with 'Discord Handle', 'mean' and 'variance' columns
                     new_row = {'Discord Handle':i, 'mean':new_ratings[j][0].mu,'variance':new_ratings[j][0].sigma}
@@ -304,7 +343,7 @@ for bounty in processed_data['Challenge_url'].unique():
                     record_details.append(new_ratings[j][0].mu)
                     record_details.append(new_ratings[j][0].sigma)
                     j=j+1
-                    st.write('try block completed')
+
                 
                 else:
                     # If current key is in the 'Discord Handle' column of the 'latest_elo_score' DataFrame,
@@ -330,8 +369,7 @@ for bounty in processed_data['Challenge_url'].unique():
             # Catch any exceptions that occur in the try block
             except Exception as e:
                 # Print the exception message along with a custom message and the challenge URL
-                st.write('problem in elo_score_keys: ',e)
-                st.write('challenge_url',challenge_df['Challenge_url'].iloc[0])
+                logger.error('Error processing record details. Error: %s', e)  # Add an error statement
 
                 # Skip the current iteration and continue with the next one
                 continue
@@ -339,19 +377,34 @@ for bounty in processed_data['Challenge_url'].unique():
             # If no exceptions occur, append the 'record_details' list to the 'data_with_rank' list
             data_with_rank.append(record_details)
 
-    # Save the 'latest_elo_score' DataFrame to a CSV file
+        # Save the 'latest_elo_score' DataFrame to a CSV file
+    logger.debug('Successfully processed record details')  # Add a debugging statement
     latest_elo_score.to_csv('latest_elo_score.csv')
+    logger.info('Successfully saved latest_elo_score.csv')  # Add an info statement
     
 
-
+logger.debug('Converting data_with_rank list into DataFrame')  # Add a debugging statement
 # Convert the 'data_with_rank' list into a pandas DataFrame.
 Final_data=pd.DataFrame(data_with_rank)
 
 try:
 # Rename the columns of the DataFrame.
     Final_data.columns=['Discord Handle', 'Address', 'Dashboard link', 'Project', 'Challenge', 'Challenge_link', 'Day', 'Score', 'Variance']
-except:
+except Exception as e:
+    logger.error('Error renaming DataFrame columns. Error: %s', e)  # Add an error statement
     pass
 
 # Save the DataFrame to a CSV file.
+logger.debug('Saving DataFrame to CSV file: true_skill_sheet.csv')  # Add a debugging statement
 Final_data.to_csv('true_skill_sheet.csv')
+logger.info('Successfully saved DataFrame to CSV file: true_skill_sheet.csv')  # Add an info statement
+
+latest_score_csv = Final_data.sort_values('Day').groupby('Discord Handle',as_index=False)['Score'].last()
+latest_score_csv=pd.DataFrame(latest_score_csv)
+latest_score_csv.columns=['Discord Handle','Score']
+
+logger.debug('Saving DataFrame to CSV file: latest_score.csv')  # Add a debugging statement
+latest_score_csv.to_csv('latest_score.csv')
+logger.info('Successfully saved DataFrame to CSV file: latest_score.csv')  # Add an info statement
+
+st.write(latest_score_csv.sort_values(by='Score',ascending=False).head())
